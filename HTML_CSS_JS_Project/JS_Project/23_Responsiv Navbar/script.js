740d513817334502c5a460de1678f001baf415bb
Hardik Chavda <chavdahardik1729@gmail.com>
2026-04-06 10:56:52 +0530

    Delete HTML_CSS_JS_Project directory

--- HTML_CSS_JS_Project/JS_Project/23_Responsiv Navbar/script.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.getElementById('navbar-toggle');
-const menu = document.getElementById('navbar-menu');
-
-toggle.addEventListener('click', () => {
-  menu.classList.toggle('active');
-});
